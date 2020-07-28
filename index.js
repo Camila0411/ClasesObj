2bc06fc8b8246ffea273770479bdcd2d4a5069b7
Maria Balaguera <mcbalaguera0411@gmail.com>
2020-07-28 18:38:32 -0500

    mi primer github

---
+++ index.js
@@ -0,0 +1,47 @@
+const papa={
+name: 'Luis Ernesto',
+lastname: 'Balaguera',
+age: 60,
+genre: 'Masculino',
+};
+
+const mama={
+name: 'Jacqueline',
+lastname: 'Vives',
+age: 58,
+genre: 'Femenino',
+};
+
+const rochi={
+name: 'Rosalba',
+lastname: 'Orozco',
+age: 75,
+genre: 'Femenino',
+};
+
+const chandi1={
+'name': 'Muñeca',
+lastname: 'BrownStar',
+age: 8,
+genre: 'Femenina',
+};
+
+const chandi2={
+name: 'Candy',
+lastname: 'BlackStar',
+age: 1,
+genre: 'Femenina',
+};
+
+const adoptado={
+name: 'Luis Enrique',
+lastname: 'Balaguera',
+age: 24,
+'genre': 'Undefined JAJA no mentis el niño es hombre'
+};
+
+const MyArray = [papa,mama,rochi,chandi1,chandi2,adoptado];
+for(let contador=0; contador<=5; contador++){
+console.log(MyArray[contador])
+};
+
